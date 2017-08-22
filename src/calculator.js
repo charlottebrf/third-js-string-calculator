8a17653f6f8d2fcde58d1920fa1064c55c920589
Charlotte Fereday <cbrfereday@gmail.com>
2017-08-22 18:36:55 +0100

    Refactors functions:

--- src/calculator.js
+++ src/calculator.js
@@ -5,29 +5,29 @@ export class Calculator {
   }
 
   _delimiters(string) {
-    let splitString = this._removeThousands(string).split("")
-    this._checkForNegatives(splitString)
-    let maybeIntegers = this._converter(splitString)
+    let characters = this._removeThousands(string).split("")
+    this._checkForNegatives(characters)
+    let maybeIntegers = this._converter(characters)
     return maybeIntegers.filter((maybeIntegers) => Number.isInteger(maybeIntegers))
   }
-  
-  _converter(splitString) {
-    let maybeIntegers = []
-    splitString.forEach(char => {
-      if ( !char ) {
-        maybeIntegers.push(0);
-      } else {
-        maybeIntegers.push(parseInt(char))
-      }
-    })
-    return maybeIntegers
+
+  _converter(characters) {
+    return characters.reduce((integers, char) => integers.concat(this._number(char)), [])
   }
 
-  _checkForNegatives(splitString) {
+  _number(char) {
+    if (!char) {
+      return 0
+    } else {
+      return parseInt(char)
+    }
+  }
+
+  _checkForNegatives(characters) {
     let negatives = []
-    for (const [index, value] of splitString.entries()) {
+    for (const [index, value] of characters.entries()) {
       if (value == "-") {
-        negatives.push('-' + splitString[index + 1])
+        negatives.push('-' + characters[index + 1])
       }
     }
     if (negatives.length > 0) {
